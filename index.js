2d40fb0cf51cdcc15d751b362a317dbc6fba848e
Terence Hughes <Terencehughes450@gmail.com>
2024-01-14 18:34:42 -0500

    added query selectors to all id's

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+//stats
 let xp = 0;
 let health = 100;
 let gold = 50;
-
+//combat
 let currentWeapon = 0;
 let fighting;
-
+//combat2
 let monsterHealth;
 let inventory = ["stick","dagger","sword"];
-
+//buttons
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector('#button2');
 const button3= document.querySelector('#button3');
+//game id's
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
