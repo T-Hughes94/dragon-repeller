7b452d7a1ccc93d4457431b2f49ea401890562a9
Terence Hughes <Terencehughes450@gmail.com>
2024-01-14 18:48:52 -0500

    initialized buttons with onclick

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//GAME VARIABLES
 //stats
 let xp = 0;
 let health = 100;
@@ -19,3 +20,21 @@ const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+//initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+//GAME FUNCTIONS
+function goStore(){
+    console.log("Going to store.");
+}
+
+function goCave(){
+    console.log("Going to cave.");
+}
+
+function fightDragon(){
+    console.log("Fighting dragon.");
+  }
