5951458ed9814641ff7422f805128d13c4b42172
Terence Hughes <Terencehughes450@gmail.com>
2024-01-16 15:47:51 -0500

    the feature will now break the weapon if it lands

--- index.js
+++ index.js
@@ -195,8 +195,9 @@ function buyHealth(){
     }else if(monsterHealth <= 0){
       fighting === 2 ? winGame() : defeatMonster();
     }
-    if(Math.random() <= .1){
+    if(Math.random() <= .1 && inventory.length !== 1){
       text.innerText += " Your " + inventory.pop() + " breaks."
+      currentWeapon --
     }
   };
 
