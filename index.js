aa6d82c3d3ac7899e3982626470cb0733e9edd8e
Terence Hughes <Terencehughes450@gmail.com>
2024-01-16 14:25:18 -0500

    re factored the health logic in the attack function to set health equal to health minus the return value of the getMonsterAttackValue function, and pass the level of the monster as an argument.

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ function buyHealth(){
   function attack(){
     text.innerText = "The " + monsters[fighting].name + " attacks."
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + "."
-    health -= monsters[fighting].level;
+    health -= getMonsterAttackValue(monsters[fighting].level);
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
