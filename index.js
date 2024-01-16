1669358d6fc68b1c741fa44967030392feee0817
Terence Hughes <Terencehughes450@gmail.com>
2024-01-16 14:42:35 -0500

    moved logic for monsterHealth into the isMonsterHit if statement

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ function buyHealth(){
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + "."
     health -= getMonsterAttackValue(monsters[fighting].level);
     if(isMonsterHit()){
-    
+      monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1
     };
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
