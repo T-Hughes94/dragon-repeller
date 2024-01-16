7567289e6614cf47be2cfef46da21a766c3f978b
Terence Hughes <Terencehughes450@gmail.com>
2024-01-16 13:52:48 -0500

    added a 5th location for returning to town and collecting gold and experience after defeating a monster by calling the defeatMonster function

--- index.js
+++ index.js
@@ -64,6 +64,12 @@ const locations = [{
     "button text": ["Attack", "Dodge", "Run"],
     "button functions": [attack, dodge, goTown],
     text: "You are fighting a monster."
+  },
+  {
+    name: "kill monster",
+    "button text": ["Go to town square", "Go to town square", "Go to town square"],
+    "button functions": [goTown, goTown, goTown],
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
   }
 ];
 //initialize buttons for functions
@@ -72,6 +78,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 //update location
 function update(location){
+    monsterStats.style.display = "none";
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
     button3.innerText = location["button text"][2];
