b1e10477ace0de70872a12228440bba863fd8d9c
Terence Hughes <Terencehughes450@gmail.com>
2024-01-16 16:08:36 -0500

    added logic for the pick function for the easter egg game.

--- index.js
+++ index.js
@@ -82,6 +82,12 @@ const locations = [{
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
     text: "You defeat the dragon! YOU WIN THE GAME! 🎉"
+  },
+  {
+    name: "easter egg",
+    "button text": ["2","8","Go to town square?"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
   }
 ];
 //initialize buttons for functions
@@ -244,8 +250,20 @@ function buyHealth(){
 
   function easterEgg(){
     update(locations[7]);
-  }
+  };
 
-  function pick(guess){
+  function pickTwo(){
+    pick(2)
+  };
   
-  }
+  function pickEight(){
+    pick(8)
+  };
+  
+  function pick(guess){
+    const numbers = []
+    while(numbers.length < 10){
+      numbers.push(Math.floor(Math.random() * 11));
+    }
+    text.innerText = "You picked " + guess + ". Here are the random numbers:"
+  };
